bbb1a36d00221086220526594187f89d4403d898
Vid Luther <vid@luther.io>
2023-12-21 22:48:33 -0600

    Remove astro-imagetools integration and replace Img component with img tag in index.astro
    The commit message is: "Remove astro-imagetools integration and replace Img component"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    astroImageTools,
     tailwind({
       applyBaseStyles: true,
     }),

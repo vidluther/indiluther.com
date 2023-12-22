198f7c42fa19388f3369b052043945a70c4b3c5c
Vid Luther <vid@luther.io>
2023-12-21 23:20:01 -0600

    Update Tailwind configuration
    - Remove screen sizes and color definitions from the theme
    - Update font families for sans-serif and serif styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,23 +2,6 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
       serif: ["Merriweather", "serif"],
